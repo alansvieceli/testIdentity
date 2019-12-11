b04eb1f8740de5e9965bcd33a2c168f911613bcf
Alan Vieceli <alan.vieceli@softplan.com.br>
2019-12-11 13:47:10 -0300

    Ajustes

--- Startup.cs
+++ Startup.cs
@@ -32,8 +32,9 @@ namespace testIdentity
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            
             services.AddControllersWithViews();
-           services.AddRazorPages();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
